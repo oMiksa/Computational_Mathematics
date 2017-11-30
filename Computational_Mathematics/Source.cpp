b2f45219cc0eab9b6af9593e2cfbb8dab1cad7a5
Smorodin Mikhail <zis.is.kiss@gmail.com>
2017-11-30 18:08:05 +0700

    end

--- Computational_Mathematics/Source.cpp
+++ Computational_Mathematics/Source.cpp
@@ -1,50 +1,71 @@
-#include<iostream>
+п»ї#include<iostream>
 #include<math.h>
 
 using namespace std;
 
-double f(double, double);//функция
-double Q();//Q интеграл
-double Qf(double);//f(x) для Q
-double du(double, double, double);//ДУ
-double li(double, double, double, double, double);//интерполяция
+double f(double, double);
+double Qf(double);
+double du(double, double, double);
+double li(double, double, double, double, double);
+double** dy(double, double, double, double, double);
+double Q(double*);
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 
-	double arrDU1[5], arrDU2[5];
+
 	double h1(0.5), h2(0.25);
 	double x(0), y(0.1);
+	double a(0), b(1);
 
-	//шаг 0.5 + Линейная инторполяция
-	arrDU1[0] = 0.1;
-	arrDU1[2] = du(x, y, h1);
-	arrDU1[4] = du((x + h1), arrDU1[2], h1);
-
-	//Линейная интерполяция
-	arrDU1[1] = li(x, y, (x + h1), arrDU1[2], h1 / 2);
-	arrDU1[3] = li(x + h1, arrDU1[2], (x + 2 * h1), arrDU1[4], h1 / 2 * 3);
-
-	// шаг 0.25
-	arrDU2[0] = 0.1;
-	arrDU2[1] = du(x, y, h2);
-	for (int i(2); i < 5; i++)
-	{
-		arrDU2[i] = du((x + h2*(double)i), arrDU2[i - 1], h2);
-	}
-
-	cout << "Количество теплоты выделяющееся на единичном сопротивлении за единицу времени Q = " << Q() << endl;
-
-	//Вывод таблица ДУ с интервалом [0,1].
-	cout << "Таблица ДУ с интервалом [0,1]" << endl;
+	double **D;
+	D = dy(x, y, a, b, h1);
+	cout << "DU y` = cos(y) / (4 + x) + y;  y(0) = 0.1 \nQ = integral(1,0) y^2 " << endl;
+	cout << endl << "Differential solution by Runge-Kutta method of the fourth order with an accuracy of 0.0001,\nThe integral of computation Simpson's formula with a step of 0.1." << endl;
 	for (int i(0); i < 5; i++)
 	{
-		cout << (i)*0.25 << " | " << arrDU1[i] << " | " << arrDU2[i] << " |";
-		if (abs(abs(arrDU1[i]) - abs(arrDU2[i])) < 0.0001 * 15)
-			cout << " Good " << endl;
-		else cout << "Bed" << endl;
+		cout << "x= " << D[0][i] << "	y= " << D[1][i] << endl;
 	}
+
+	double arrli[2][11];
+	for (int i(0); i < 11; i++)
+	{
+		arrli[0][i] = (double)i*0.1;
+	}
+	for (int i(0); i < 11; i++)
+	{
+		if (arrli[0][i] == D[0][i])
+		{
+			arrli[1][i] = D[1][i];
+			continue;
+		}
+
+		int j(0);
+		while (D[0][j] < arrli[0][i])
+		{
+			j++;
+		}
+		arrli[1][i] = li(D[0][j - 1], D[1][j - 1], D[0][j], D[1][j], arrli[0][i]);
+	}
+	cout << endl << "Find the values of the function in intermediate nodes use linear interpolation" << endl;
+	for (int i(0); i < 11; i++)
+	{
+		cout << "x= " << arrli[0][i] << "	y= " << arrli[1][i] << endl;
+	}
+
+	double arrQ[11];
+	for (int i(0); i < 11; i++)
+	{
+		arrQ[i] = arrli[1][i];
+	}
+
+
+	cout << endl << "The amount of heat released per unit time\nQ = " << Q(arrQ) << endl;
+
+
+
+
 
 	system("pause");
 	return 0;
@@ -52,21 +73,23 @@ int main()
 
 double f(double x, double y)
 {
-	return cos(y) / (4 + x) + y;
+	return (cos(y) / (4 + x)) + y;
 }
 
-double Q()
+double Q(double *z)
 {
-	double h(0.1), b(1), a(0), sum(Qf(a) + Qf(b));
-	double n = (b - a) / h;
-	for (double i(0); i < n; i++)
+	double h(0.1), a(0), b(1);
+	int n = (b - a) / h;
+	double sum(Qf(z[0]) + Qf(z[n]));
+
+	for (int i(1); i < n; i++)
 	{
 		if ((int)i % 2)
 		{
-			sum += 4 * Qf(h*i);
+			sum += 4 * Qf(z[i]);
 		}
 		else {
-			sum += 2 * Qf(h*i);
+			sum += 2 * Qf(z[i]);
 		}
 	}
 	return h / 3 * sum;
@@ -81,13 +104,62 @@ double du(double x0, double y0, double h)
 {
 	double k1, k2, k3, k4;
 	k1 = f(x0, y0);
-	k2 = f(x0 + h / 2, y0 + h / 2 * k1);
-	k3 = f(x0 + h / 2, y0 + h / 2 * k2);
+	k2 = f(x0 + h / 2.0, y0 + h / 2.0 * k1);
+	k3 = f(x0 + h / 2.0, y0 + h / 2.0 * k2);
 	k4 = f(x0 + h, y0 + h * k3);
-	return y0 + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+	return y0 + (h / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4));
 }
 
 double li(double x1, double y1, double x2, double y2, double X)
 {
 	return y1 + (y2 - y1) / (x2 - x1)*(X - x1);
+}
+
+double** dy(double x, double y, double a, double b, double h)
+{
+	double **arr1 = new double*[2];
+	for (int i(0); i < 2; i++)
+		arr1[i] = new double[(b - a) / h];
+
+	double **arr2 = new double*[2];
+	for (int i(0); i < 2; i++)
+		arr2[i] = new double[(b - a) / h*2.0];
+
+
+	arr1[0][0] = arr2[0][0] = 0;
+	arr1[1][0] = arr2[1][0] = 0.1;
+
+	for (int i(1); i < (b - a) / h * 2.0 + 1; i++)
+	{
+		arr2[0][i] = x + i*(h / 2.0);
+		arr2[1][i] = du(arr2[0][i - 1], arr2[1][i - 1], h / 2.0);
+	}
+
+	for (int i(1); i < (b - a) / h + 1; i++)
+	{
+		arr1[0][i] = x + i*h;
+		arr1[1][i] = du(arr1[0][i - 1], arr1[1][i - 1], h);
+	}
+
+	/*
+	cout << endl << " x		 y	 " << endl;
+	for (int i(0); i < (b - a) / h+1; i++)
+	{
+	cout << i*h << ") " << arrX1[i] << "		 " << arrY1[i] << endl;
+	}
+	cout << endl << " x		 y	 " << endl;
+	for (int i(0); i < (b - a) / h * 2+1; i++)
+	{
+	cout << i*h/2 << ") " << arrX2[i] << "		 " << arrY2[i] << endl;
+	}
+	*/
+
+	for (int i(0); i < (b - a) / h + 1; i++)
+	{
+		if (arr1[1][i] - arr2[1][i * 2 + 1] < 0.0001)
+		{
+			return arr2;
+		}
+	}
+	dy(x, y, a, b, h / 2.0);
 }
--- lab4/Source.cpp
+++ lab4/Source.cpp
@@ -3,27 +3,51 @@
 
 using namespace std;
 
-double Vareant(double);
+double Var(double);
 double Step(double);
+double f(double, double);
+double fi(double, double, double);
+double fii(double, double);
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 
-	double c(Vareant(2.0));
+	double c(Var(2.0));
 	double h(Step(c));
 
-	double arr[3][22];
-	cout << "numerical differentiation" << endl;
-	cout << "|	h		|	f(x)	|	f`(x)	|" << endl;
+	double arr[2][22];
+	double arri[3][20];
 
+
+	for (int i(-1); i < 21; i++)
+	{
+		arr[0][i + 1] = c + i*h;
+		arr[1][i + 1] = f((arr[0][i + 1]), c);
+	}
+
+	for (int i(0); i < 20; i++)
+	{
+		arri[0][i] = c + i*h;
+		arri[1][i] = fi(arr[1][i + 2], arr[1][i], h);
+		arri[2][i] = fii(arri[0][i], c);
+	}
+
+
+	cout << " differentiation " << endl;
+	cout << "\n x		|	f	" << endl;
+	cout << fixed << right;
 	for (int i(0); i < 22; i++)
 	{
-		arr[0][i] = c + h*((double)i - 1.0);//интервал с h
-		arr[1][i] = (1 / pow(c, 2))*cos(c*arr[0][i]);// f(x)
-		arr[2][i] = (-1 / c)*cos(c*arr[0][i]);//f`(x)
-		cout << "| " << arr[0][i] << "	|	" << arr[1][i] << "	|	" << arr[2][i] << "	|" << endl;
+		cout << arr[0][i] << "	|  " << arr[1][i] << "  " << endl;
 	}
+
+	cout << "\n x`		|	f`	|	f``" << endl;
+	for (int i(0); i < 20; i++)
+	{
+		cout << arri[0][i] << "	|  " << arri[1][i] << "	|	" << arri[2][i] << endl;
+	}
+
 
 
 
@@ -31,12 +55,29 @@ int main()
 	return 0;
 }
 
-double Vareant(double n)
+double Var(double n)
 {
+	//option
 	return 3 * pow(0.1*(n + 1.0), 3);
 }
 
 double Step(double a)
 {
 	return pow((3 * 0.0001) / a, (1.0 / 3.0));
+}
+double f(double x, double c)
+{
+	//function
+	return 1 / pow(c, 2)*cos(c*x);
+}
+double fi(double x1, double x2, double h)
+{
+	//approximate function
+	return (x1 - x2) / (2.0 * h);
+}
+
+double fii(double x, double c)
+{
+	//exact function
+	return (-1) / c*sin(c*x);
 }
